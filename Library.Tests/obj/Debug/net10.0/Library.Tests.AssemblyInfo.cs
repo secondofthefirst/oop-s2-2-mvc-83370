0c9da9df5c2cd0d2c82bab8087ff7dd95ad802b8
raphael noel-jean <github.com.tinsel401@passmail.com>
2026-03-31 13:57:34 +0200

    afzr

--- Library.Tests/obj/Debug/net10.0/Library.Tests.AssemblyInfo.cs
+++ Library.Tests/obj/Debug/net10.0/Library.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e812ea4763e7bfa65fc07950af944bb643648b5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c026ef8ec38aff536a1e4ff9b880c2844109e74e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
